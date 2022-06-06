b45d241db9a150727805f056b900332e7ee9e641
idle947 <idle947@gmail.com>
2022-06-06 12:53:39 +0900

    연습코드

--- routers/download.py
+++ routers/download.py
@@ -14,16 +14,47 @@ async def main(request: Request):
 async def get_playlist(request: Request):
     ydl_opts = {
         'ignoreerrors': True,
+        'skip_download':True,
+        'extractor_args':{'youtube':{'player_client':['web']}},
+        'flat-playlist':True,
     }
     with YoutubeDL(ydl_opts) as ydl:
 
-        res = ydl.extract_info("https://www.youtube.com/watch?v=r97Wb-KLiKU", download=False)
-        for i in res["formats"]:
-            if i["audio_ext"] != "none":
-                if i["format_note"] == "medium":
-                    return HTMLResponse(f"""
-                    <html> <a href="{i['url']}">다운로드</a> </html>
-                    """)
+        res = ydl.extract_info("https://www.youtube.com/playlist?list=PLX3CrwbL_r9andFCCGev0-R4ejxxWfQVH", download=False)
+
+        # for i in res["formats"]:
+        #     if i["audio_ext"] != "none":
+        #         if i["format_note"] == "medium":
+        #             return HTMLResponse(f"""
+        #             <html> <a href="{i['url']}">다운로드</a> </html>
+        #             """)
 
 
-        return res["formats"]
+        return res
+
+
+if __name__ == '__main__':
+
+    def get_playlist():
+        ydl_opts = {
+            'ignoreerrors': True,
+            'skip_download': True,
+            'extractor_args': {'youtube':
+                                   {'player_client':['android'],'max_comments':[0,0,0,0],'player_skip':['webpage']},
+                               'youtubetab':{'skip':['webpage']}},
+            'flat-playlist': True,
+        }
+        with YoutubeDL(ydl_opts) as ydl:
+            res = ydl.extract_info("https://www.youtube.com/playlist?list=PLX3CrwbL_r9andFCCGev0-R4ejxxWfQVH",
+                                   download=False)
+
+            # for i in res["formats"]:
+            #     if i["audio_ext"] != "none":
+            #         if i["format_note"] == "medium":
+            #             return HTMLResponse(f"""
+            #             <html> <a href="{i['url']}">다운로드</a> </html>
+            #             """)
+
+            return res
+    res = get_playlist()
+    print(res)
